22b2b460489f1ff3b4763a0ab6ccc73237b59250
Segamaya <Segamaya>
2016-06-08 23:31:30 -0500

    Adicion del objeto y el Json completo

--- process/typescript/app.component.js
+++ process/typescript/app.component.js
@@ -11,7 +11,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, CREWMEMBERS_INFO;
     return {
         setters:[
             function (core_1_1) {
@@ -20,17 +20,40 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.info = CREWMEMBERS_INFO;
+                    this.preferenceFound = null;
                 }
+                //crewmemberID:string;
+                //preference:string;
+                /*
+                  constructor(){
+                    this.crewmemberID='a1';
+                    this.preference='Blue';
+                  }
+                */
+                AppComponent.prototype.sendId = function (preferenceID) {
+                    console.log(preferenceID);
+                    for (var i = 0; i < this.info.length; i++) {
+                        if (this.info[i].id == preferenceID) {
+                            console.log(this.info[i].preference);
+                            return this.info[i].preference;
+                        }
+                    }
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'crewmemberData',
                         templateUrl: 'partials/app.html',
+                        styleUrls: ['css/app.css']
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            CREWMEMBERS_INFO = [
+                { "id": "a3", "preference": "Red" }, { "id": "a1", "preference": "Blue" }, { "id": "a4", "preference": "Green" }, { "id": "a2", "preference": "Yellow" }
+            ];
         }
     }
 });
--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -1,11 +1,46 @@
 import {Component} from 'angular2/core';
 
+interface CrewMemberInfo{
+  id:string;
+  preference:string;
+
+}
+
 
 @Component({
   selector: 'crewmemberData',
   templateUrl: 'partials/app.html',
+  styleUrls: ['css/app.css']
 })
 
 export class AppComponent {
 
+  info=CREWMEMBERS_INFO;
+  preferenceFound=null;
+
+  //crewmemberID:string;
+  //preference:string;
+/*
+  constructor(){
+    this.crewmemberID='a1';
+    this.preference='Blue';
+  }
+*/
+  sendId(preferenceID){
+    console.log(preferenceID)
+    for(var i = 0; i < this.info.length; i++)
+  {
+    if(this.info[i].id == preferenceID)
+  {
+    console.log(this.info[i].preference)
+    return this.info[i].preference;
+
+  }
+  }
+  }
+
 }
+
+var CREWMEMBERS_INFO:CrewMemberInfo[]=   [
+   { "id" : "a3", "preference" : "Red" }, { "id" : "a1", "preference" : "Blue" }, { "id" : "a4", "preference" : "Green" }, { "id" : "a2", "preference" : "Yellow" }
+ ]
