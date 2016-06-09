3c3e83dacfdb2d98d9d9772179e6291612a04bc7
x216259 <sergio.amaya@wnco.com>
2016-06-08 21:32:35 -0500

    creating a separate component

--- process/typescript/app.component.js
+++ process/typescript/app.component.js
@@ -24,7 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'crewmemberData',
-                        template: '<h1>COMO VAMOS</h1>'
+                        templateUrl: 'partials/app.html',
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'crewmemberData',
-  template: '<h1>COMO VAMOS</h1>'
+  templateUrl: 'partials/app.html',
 })
 
 export class AppComponent {
